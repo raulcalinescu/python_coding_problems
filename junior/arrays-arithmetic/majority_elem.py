58ade53c02bed6557591221ffeddc56d181c535e
raulcalinescu <raul.salajan@gmail.com>
2021-09-28 20:34:06 -0500

    majority_element solved with Randomization and efficient HashMap

--- junior/arrays-arithmetic/majority_elem.py
+++ junior/arrays-arithmetic/majority_elem.py
@@ -15,15 +15,16 @@
 # float('inf')
 # in similar fashion, a number smaller than all others:
 # float('-inf')
-
+import collections
+import random
 from collections import defaultdict
 from typing import List
 import sys
 
 
 class Solution(object):
-    @staticmethod
-    def majority_element(nums: List[int]) -> int:
+    @staticmethod # O(n), O(n)
+    def majority_element_mysol(nums: List[int]) -> int:
         # max = -sys.maxsize or float('-inf')
         max = 0
         dict = defaultdict(int)
@@ -34,6 +35,28 @@ class Solution(object):
                 max = dict[k]
                 result = k
         return result
+
+    # O(n), O(n)
+    def maj_elem_hash_map(self, nums: List[int]) -> int:
+        counts = collections.Counter(nums)  # Counter holds data in an unordered collection just like hashtable obj
+                                            # elements represent the keys and the count as values
+        return max(counts.keys(), key=counts.get)
+
+    # O(infinity) - no upper bound for worst case, but likely linear otherwise and O(1) space
+    def maj_elem_randomization(self, nums: List[int]) -> int:
+        maj_count = len(nums) // 2
+        while True:
+            candidate = random.choice(nums)
+            if sum(1 for elem in nums if elem == candidate) > maj_count:
+                return candidate
+
+    # O(n^2) time and O(1) space, no additional space proportional to the input size
+    def majority_element_brutef(self, nums: List[int]) -> int:
+        majority_count = len(nums)//2
+        for num in nums:
+            count = sum(1 for elem in nums if elem == num)
+            if count > majority_count:
+                return num
 
 
 # Driver code
